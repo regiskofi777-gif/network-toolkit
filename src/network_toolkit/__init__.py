bd5942a776f4bd983b0da776a21d7a72cf65d892
regiskofi777-gif <regiskofi777@gmail.com>
2026-09-25 22:41:02 +0000

    feat: add package entry point

---
+++ src/network_toolkit/__init__.py
@@ -0,0 +1,2 @@
+"""Network Toolkit - outil de scan et de diagnostic réseau."""
+__version__ = "0.1.0"
---
+++ src/network_toolkit/__main__.py
@@ -0,0 +1,6 @@
+"""Le point d'entrée : """
+
+from network_toolkit.cli import run
+
+if __name__ == "__main__":
+    run()
